97ca756020a1938d72d2edc16c6809383d5cdb90
DTkach <DTkach3@hotmail.com>
2015-10-05 17:48:27 -0400

    Coding Commit
    Will it work? Have I lost all ability to use GitHub? Find out on the
    next episode of Java Programming Z

--- src/howdryiam/HowDRYIAm.java
+++ src/howdryiam/HowDRYIAm.java
@@ -11,18 +11,10 @@ public class HowDRYIAm {
 	Scanner keyboard = new Scanner(System.in);
 	
 	// Dimensions Prep
-	double width;
-	double depth;
-	double height;
 	Dimensions dim1;
 	Dimensions dim2;
 	
 	// address Prep
-	String name;
-	String street;
-	String state;
-	String city;
-	int zip;
 	Address address1;
 	Address address2;
 		
@@ -32,57 +24,21 @@ public class HowDRYIAm {
 	
 	// Preparing First Box
 	
-	println("Constructing Address one");
-	println("Attn:");
-	name = keyboard.next();
-	println("Street:");
-	street = keyboard.next();
-	println("City:");
-	city = keyboard.next();
-	println("State:");
-	state = keyboard.next();
-	println("ZipCode:");
-	zip = keyboard.nextInt();
-	
-	address1 = new Address(city, street, city, state, zip);
+        println("Constructing Address one");
+	address1 = constructAddress(keyboard);
 	
 	println("Constructing Box one");
-	println("Height:");
-	height = keyboard.nextDouble();
-	println("Width:");
-	width = keyboard.nextDouble();
-	println("Depth:");
-	depth = keyboard.nextDouble();
-	
-	dim1 = new Dimensions(width, height, depth);
+	dim1 = constructDimensions(keyboard);
 	
 	label1 = new LabelPrinter(address1, dim1);
 	
 	// Preparing Second Box
 	
-	println("Constructing Address two");
-	println("Attn:");
-	name = keyboard.next();
-	println("Street:");
-	street = keyboard.next();
-	println("City:");
-	city = keyboard.next();
-	println("State:");
-	state = keyboard.next();
-	println("ZipCode:");
-	zip = keyboard.nextInt();
-	
-	address2 = new Address(city, street, city, state, zip);
+        println("Constructing Address two");
+	address2 = constructAddress(keyboard);
 	
 	println("Constructing Box two");
-	println("Height:");
-	height = keyboard.nextDouble();
-	println("Width:");
-	width = keyboard.nextDouble();
-	println("Depth:");
-	depth = keyboard.nextDouble();
-	
-	dim2 = new Dimensions(width, height, depth);
+	dim2 = constructDimensions(keyboard);
 	
 	label2 = new LabelPrinter(address2, dim2);
 	
@@ -94,5 +50,54 @@ public class HowDRYIAm {
   
   public static void println(String message){
 	System.out.println(message);
+  }
+  
+  /**
+   * Constructs an Address instance from user input
+   * @param keyboard - A Scanner object to provide values for the creation
+   * of an Address
+   * @return Address instance created from user input
+   */
+  public static Address constructAddress(Scanner keyboard){
+      	String name;
+	String street;
+	String state;
+	String city;
+	int zip;
+        
+	println("Attn:");
+	name = keyboard.next();
+	println("Street:");
+	street = keyboard.next();
+	println("City:");
+	city = keyboard.next();
+	println("State:");
+	state = keyboard.next();
+	println("ZipCode:");
+	zip = keyboard.nextInt();
+        
+        return new Address(name, street, city, state, zip);
+        
+  }
+  
+   /**
+   * Constructs a Dimensions instance from user input
+   * @param keyboard - A Scanner object to provide values for the creation
+   * of a set of Dimensions
+   * @return Dimensions instance created from user input
+   */
+  public static Dimensions constructDimensions(Scanner keyboard){
+      	double width;
+	double depth;
+	double height;
+        
+      	println("Height:");
+	height = keyboard.nextDouble();
+	println("Width:");
+	width = keyboard.nextDouble();
+	println("Depth:");
+	depth = keyboard.nextDouble();
+        
+        return new Dimensions(width, height, depth);
   }
 }
