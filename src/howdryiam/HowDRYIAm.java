cb68d7c574bae8857e2d40d3ef5bc0c2c88d6d35
Zachary Murphy <murphyz@my.wccc.edu>
2015-10-05 20:35:23 -0400

    initial commit
    condensed it into methods to make it more cleaner. little odd the way it
    runs, i think i got the hang of methods alot more better.

--- src/howdryiam/HowDRYIAm.java
+++ src/howdryiam/HowDRYIAm.java
@@ -7,84 +7,63 @@ public class HowDRYIAm {
   /**
    * @param args the command line arguments
    */
+    
+ public static Address readAddressInfo(){
+	Scanner keyboard = new Scanner(System.in);
+          
+     	println("Constructing Address one");
+	println("Attn:");
+        String name = "HI";
+	//String name = keyboard.next();
+	println("Street:");
+	String street = keyboard.next();
+	println("City:");
+	String city = keyboard.next();
+	println("State:");
+	String state = keyboard.next();
+	println("ZipCode:");
+	int zip = keyboard.nextInt();
+     
+     	Address address = new Address(city, street, city, state, zip);
+     return address;
+     
+
+     
+ }
+ 
+ public static Dimensions readDimensionInfor(){
+     	Scanner keyboard = new Scanner(System.in);
+	
+        println("Constructing Box one");
+	println("Height:");
+	double height = keyboard.nextDouble();
+	println("Width:");
+	double width = keyboard.nextDouble();
+	println("Depth:");
+	double depth = keyboard.nextDouble();
+	
+	Dimensions dim = new Dimensions(width, height, depth);
+        
+    return dim;
+ }
   public static void main(String[] args) {
 	Scanner keyboard = new Scanner(System.in);
-	
-	// Dimensions Prep
-	double width;
-	double depth;
-	double height;
-	Dimensions dim1;
-	Dimensions dim2;
-	
-	// address Prep
-	String name;
-	String street;
-	String state;
-	String city;
-	int zip;
-	Address address1;
-	Address address2;
-		
+
 	
 	LabelPrinter label1;
 	LabelPrinter label2;
 	
-	// Preparing First Box
-	
-	println("Constructing Address one");
-	println("Attn:");
-	name = keyboard.next();
-	println("Street:");
-	street = keyboard.next();
-	println("City:");
-	city = keyboard.next();
-	println("State:");
-	state = keyboard.next();
-	println("ZipCode:");
-	zip = keyboard.nextInt();
-	
-	address1 = new Address(city, street, city, state, zip);
-	
-	println("Constructing Box one");
-	println("Height:");
-	height = keyboard.nextDouble();
-	println("Width:");
-	width = keyboard.nextDouble();
-	println("Depth:");
-	depth = keyboard.nextDouble();
-	
-	dim1 = new Dimensions(width, height, depth);
-	
-	label1 = new LabelPrinter(address1, dim1);
-	
-	// Preparing Second Box
-	
-	println("Constructing Address two");
-	println("Attn:");
-	name = keyboard.next();
-	println("Street:");
-	street = keyboard.next();
-	println("City:");
-	city = keyboard.next();
-	println("State:");
-	state = keyboard.next();
-	println("ZipCode:");
-	zip = keyboard.nextInt();
-	
-	address2 = new Address(city, street, city, state, zip);
-	
-	println("Constructing Box two");
-	println("Height:");
-	height = keyboard.nextDouble();
-	println("Width:");
-	width = keyboard.nextDouble();
-	println("Depth:");
-	depth = keyboard.nextDouble();
-	
-	dim2 = new Dimensions(width, height, depth);
-	
+        //recalling address for address one with return
+	Address address1 = readAddressInfo();
+        //recalling address for address two with return
+        Address address2 = readAddressInfo();
+        //recalling the dimensions for box 1 
+        Dimensions dim1 = readDimensionInfor();
+        label1 = new LabelPrinter(address1, dim1);
+        //recalling dimensions for box 2
+        Dimensions dim2= readDimensionInfor();
 	label2 = new LabelPrinter(address2, dim2);
+
 	
 	//Printing Labels
 	label1.printLabel();
